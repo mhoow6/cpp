6077aa64b2a349a2149d050f6a9df04d0a8715f3
DESKTOP-FGAJHSJ\mhoow <mhoow6@gmail.com>
2021-07-18 21:55:14 +0900

    헤더가드 연습

---
+++ basic/header_guard/ExampleHeader_1.h
@@ -0,0 +1,13 @@
+#ifndef EXAMPLEHEADER_1_H // EXAMPLEHEADER_1_H 정의 되있지 않을 경우
+#define EXAMPLEHEADER_1_H 
+
+int getNumberOne()
+{
+	return 1;
+}
+
+#endif // !EXAMPLEHEADER_1_H
+
+// 현재 Visual Studio에서는 #pragma once 로 단순한 헤더가드를 지원한다.
+
+
---
+++ basic/header_guard/HeaderGuardExample.cpp
@@ -0,0 +1,22 @@
+#include "ExampleHeader_1.h"
+#include "ExampleHeader_2.h"
+
+/* 
+[예시] 헤더가드가 없으면 전처리기는 결국..
+
+int getNumberOne()
+{
+	return 1;
+}
+
+int getNumberOne()
+{
+	return 1;
+}
+
+*/
+
+int main()
+{
+	return 0;
+}
